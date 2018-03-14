0d43555b056fa7c28be0425bf4d0b88a3de43885
antoniomm <antonio.martinmoreno@zooplus.com>
2018-03-14 17:42:24 +0100

    new components card and grid

--- src/components/container/Container.vue
+++ src/components/container/Container.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="container">
     <my-section></my-section>
+    <my-grid></my-grid>
   </div>
 </template>
 <script>
 import mySection from './Section'
+import myGrid from './Grid'
 export default {
   name: 'mycontainer',
-  components: {'my-section': mySection}
+  components: {
+    'my-section': mySection,
+    'my-grid': myGrid
+  }
 }
 </script>
 <style>
---
+++ src/components/container/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="columns" >
+    <my-card v-for="(item, key, index) in items" :key="index" ></my-card>
+  </div>
+</template>
+<script>
+import myCard from './Card'
+export default {
+  name: 'myGrid',
+  components: {
+    'my-card': myCard
+  },
+  data () {
+    return {
+      items: {
+        1: 'hello1',
+        2: 'hello2',
+        3: 'hello3',
+        4: 'hello4'
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
